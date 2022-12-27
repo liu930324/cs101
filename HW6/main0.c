856bb97d1e5cb18b8554247b78fd4e58744de7b4
liu930324 <113400161+liu930324@users.noreply.github.com>
2022-12-28 01:12:12 +0800

    Update main0.c

--- HW6/main0.c
+++ HW6/main0.c
@@ -1,1 +1,15 @@
+int main() {
+    int i =10;
+  
+    if(i%15==0) {
+      printf("Love IU");
+    }
+    else if(i%5==0) {
+      printf("IU");
+    }  
+    else if(i%5==0) {
+      printf("Love");
+    }
+    return 0;
+}
 
