9b999b4e35c6bda12eeb13a78b82e89936a0f23b
liu930324 <113400161+liu930324@users.noreply.github.com>
2022-12-29 00:39:53 +0800

    Update main1.c

--- HW7/main1.c
+++ HW7/main1.c
@@ -1,1 +1,18 @@
+int main()
+{
+    int i =7;
+    
+    for(int n=1;n<=i;n++){
+        for(int p=1;p<=i;p++){
+            if(p<=i-n){
+                printf(" ");
+            }
+            else{
+                printf("%d ",n);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
