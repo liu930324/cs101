90f6f28c129681ddc3b188f5b3de5b8283b7a925
liu930324 <113400161+liu930324@users.noreply.github.com>
2022-12-29 16:55:18 +0800

    Update main.c

--- qz2/main.c
+++ qz2/main.c
@@ -1,5 +1,5 @@
 void print_spaces(int r, int rows){
-    for(int r = 1,r <= i,r++){
+    for(int r = 1,r <= i3,r++){
         printf(" ");
     }
 }
@@ -7,12 +7,13 @@ void print_stars(int r){
     for(int r = 1,r<=rows,r++){
         printf("*");
     }
+}
 int main(){
     int rows = 5;
-    for(int i3 =1,i3<=rows,i3++){
+    for(int i =1,i<=rows,i++){
         print_spaces(i3);
         print_stars(i3);
+        printf("\n")
     }
     return 0;
 }
-
