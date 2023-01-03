a993ad8949bddf2b2a3cad792ce9b9f4ee4474be
liu930324 <113400161+liu930324@users.noreply.github.com>
2023-01-03 23:49:35 +0800

    Update main1.c

--- HW8/main1.c
+++ HW8/main1.c
@@ -1,1 +1,17 @@
+int main()
+{
+    int rows =10;
+    
+    for(int i=1;i<=rows;i++){
+        for(int space =1;space <=rows-i;space++){
+            printf(" ");
+        }
+        for(int star =1;star <=i*2-1;star++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
