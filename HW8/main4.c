507e8e025028909260e4ad90dc975fb7a18c6718
liu930324 <113400161+liu930324@users.noreply.github.com>
2023-01-04 20:39:45 +0800

    Update main4.c

--- HW8/main4.c
+++ HW8/main4.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-
 int round_func(float f){
     return ((f*2+1)/2);
 }
 int main(){
-    float f =9.18;
-    int n=round_func(f);
+    float h =9.18;
+    int n=round_func(h);
     printf("%d",n);
     
     return 0;
