a353baedd23ed0ecd54967b8913134044cdbd7bc
liu930324 <113400161+liu930324@users.noreply.github.com>
2023-01-11 19:25:56 +0800

    Update main8.c

--- HW8/main8.c
+++ HW8/main8.c
@@ -1,1 +1,35 @@
-
+void print_inverted_triangle(int n){
+    int rows =1;
+    for(int i =1;i <=n;i++){
+        for(int i =2;i <=rows;i++){
+            printf(" ");
+        }
+        for(int i=1;i <=n*2-rows*2+1;i++){
+            printf("*");
+        }
+        printf("\n");
+        rows+=1;
+    }
+}
+void print_triangle_norows1(int n){
+    int rows =2;
+    for(int i=2;i <=n;i++){
+        for(int i =1;i <=n-rows;i++){
+            printf(" ");
+        }
+        for(int i=1;i <=rows*2-1;i++){
+            printf("*");
+        }
+        printf("\n");
+        rows++;
+    }
+}
+void print_hourglass(int n){
+    print_inverted_triangle(n);
+    print_triangle_norows1(n);
+}
+int main()
+{
+    print_hourglass(4);
+    return 0;
+}
